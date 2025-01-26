93170c217695a69a8c44c900b5485bde24ea22b2
AlexDomas <aleksej.domas@mail.ru>
2025-01-26 15:06:05 +0300

    Initialization commit

---
+++ src/main/java/tech/idftechnology/domas/bankmicroservice/bankmicroservice/BankMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package tech.idftechnology.domas.bankmicroservice.bankmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/idftechnology/domas/bankmicroservice/bankmicroservice/BankMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.idftechnology.domas.bankmicroservice.bankmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
